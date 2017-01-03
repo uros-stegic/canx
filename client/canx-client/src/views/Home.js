961f0063f7d83f29a4bad5e0ca921bfd7b178249
uros-stegic <urosstegic@gmx.com>
2017-01-03 15:15:41 +0100

    Modal for logout.

--- client/canx-client/src/views/Home.js
+++ client/canx-client/src/views/Home.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import Modal from 'react-modal';
 import {Link} from 'react-router';
 import Logo from './Logo';
 
+const customStyles = {
+  content : {
+    top                   : '40%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+	width				  : '90vw',
+	height				  : '50vw',
+    transform             : 'translate(-50%, -50%)',
+	color				  : '#cccccc',
+	backgroundColor		  : '#2f3238',
+	borderRadius		  : '0'  
+}
+};
+
 class Home extends React.Component {
-   	render(){
+   	constructor(...args){
+		super(...args);
+		this.state = {
+			modalIsOpen: false
+		};	
+		this.openModal = this.openModal.bind(this);
+		this.closeModal = this.closeModal.bind(this);
+	};
+
+	openModal(){
+	 	this.setState({modalIsOpen: true});
+	};
+
+	closeModal(){
+		this.setState({modalIsOpen: false});
+	};
+	
+	render(){
 		return (
 		<div className='home-container'>
 			
@@ -27,12 +61,25 @@ class Home extends React.Component {
 						<div className='help-icon home-btn'> </div>
 	   		 		</div>
 				 </Link>
-				<Link to='/logout'>
-					<div className='home-btn-container'>
+					<div className='home-btn-container' onClick={this.openModal}>
 						<div className='logout-icon home-btn'> </div>
 	   			 	</div>
-				</Link>
-			</div>	
+			</div>
+			<Modal isOpen={this.state.modalIsOpen}
+				   onRequestClose={this.closeModal}
+				   contentLabel="Logout"
+				   shouldCloseOnOverlayClick={true}
+				   style={customStyles}
+				>
+			
+				<h5> Are you sure? </h5>
+
+				<input className='logout-modal-yes' type='button' value='Yes' onClick={this.closeModal}/> 
+
+				<input className='logout-modal-no' type='button' value='No' onClick={this.closeModal}/> 
+
+			</Modal>
+	
 		</div>
     );}
 }
