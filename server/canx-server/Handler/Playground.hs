f3142d5b916f774262bb683d72523121bef12362
uros-stegic <urosstegic@gmx.com>
2017-02-04 16:52:50 +0100

    Function for encryption w/ SHA256/HMAC

--- server/canx-server/Handler/Playground.hs
+++ server/canx-server/Handler/Playground.hs
@@ -1,25 +1,31 @@
 module Handler.Playground where
 
 import Import
-{-
-import qualified Data.ByteString
+import Numeric (showIntAtBase)
+import Data.Char (intToDigit)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as E
 import qualified Crypto.Hash.SHA256 as SHA256
+import qualified Data.ByteString as BS
 
 
-encrypt :: Text -> Text
-encrypt inp = digest
-    where
-        digest   = pack $ Data.ByteString.unpack hashed
-        hashed   = hash $ Data.ByteString.pack unpacked
-        unpacked = unpack inp
--}
+encrypt :: Text -> Text -> Text
+encrypt key str = let
+    hashed = SHA256.hmac (E.encodeUtf8 key) $ (E.encodeUtf8 str)
+    result = let
+        stringified = BS.unpack hashed
+        in foldr (\x y ->
+                showIntAtBase (16 :: Int) intToDigit (fromIntegral x) ""
+                ++ y
+            ) "" stringified
+    in T.pack result
 
-getTestR :: Text -> Handler Value
-getTestR arg = do
-    let name' = toUpper arg
-    let res = Category {
-        categoryName =  name',
-        categoryLetters = Nothing
+getTestR :: Text -> Text -> Handler Value
+getTestR key str = do
+    let hashed = encrypt key str
+    let res = Category { -- using this just to see the results of encrypt fn
+        categoryName = hashed,
+        categoryLetters = Just [key, str]
     }
     returnJson res
 
